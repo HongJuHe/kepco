d4e548baa17d463de934a2b7d5ca1d6e3281fb3f
HongJuHe <jhhong1102@naver.com>
2021-02-16 18:02:42 +0900

    kepco-gui-server final

--- routes/cmd.js
+++ routes/cmd.js
@@ -1,73 +1,70 @@
 const express = require('express');
-const {spawn} = require('child_process');
+const {spawn, exec} = require('child_process');
 const path = require('path');
 const { Z_DEFAULT_COMPRESSION } = require('zlib');
 const router = express.Router();
 const basePath = path.join(__dirname, '../', 'pyroscripts')
-//let pred_PV, smp, P2P, RT_Pnet
+
+var req_info = {
+    python : "",
+    result : undefined,
+    script : "",
+    index : "",
+    
+}
+
+var _exec;
 
 router.post('/', function(req,res, next) {
-  let index = req.body.index
-  let script = "script"+index+".py";
+
+  req_info.index = req.body.index
+  req_info.script = "script" + req_info.index + ".py";
+
   let args = []
-  args.push(index)
-  /*if(index=="KPU_10")
-  {
-    args.push(pred_PV);
-  }
-  else if(index=="KPU_11")
-  {
-    args.push(smp);
-  }
-  else if(index=="KPU_12")
-  {
-    args.push(P2P);
-  }
-  else if(index=="POSTECH_8")
-  {
-    args.push(RT_Pnet)
-  }*/
-  //args.push(eval(req.body.arguments))
-  //args.push(req.body.arguments.split(' '))
-  let exec = [path.join(basePath, script)].concat(args)
-  console.log(exec)
-  let result;
+
+  args.push(req_info.index)
+
+  _exec = [path.join(basePath, req_info.script)].concat(args)
+  console.log("_exec")
+  console.log(_exec)
+
+  
+
   // spawn new child process to call the python script
-  let python = spawn('python', exec);
+  req_info.python = spawn('python', _exec);
+
+  console.log("execute");
+  res.status(200).json({
+    script : _exec.join(' '),
+    result : req_info.result
+  });
 
   // collect data from script
-  python.stdout.on('data', function (data) {
-    console.log('Pipe result from python script ... ' + exec.join(' '));
-    result = data.toString();
-    console.log("RESULT: "+result)
-    /*if(index=="POSTECH_6")
-    {
-      pred_PV = result;
-    }
-    else if(index="KPU_10")
-    {
-      smp = result;
-    }
-    else if(index="KPU_11")
-    {
-      P2P = result;
-    }
-    else if(index="KPU_12")
-    {
-      RT_Pnet = result;
-    }*/
-  });
+  req_info.python.stdout.on('data', function (data) {
 
-  // in close event we are sure that stream from child process is closed
-  python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-
-    // send data to client
-    res.status(200).json({
-      script: exec.join(' '),
-      result: result,
-    });
+    console.log('Pipe result from python script ... ' + _exec.join(' '));
+    req_info.result = data.toString();
+    console.log("RESULT: "+req_info.result);
+    return;
   });
 })
+
+router.get('/', function(req, res, next){
+
+  console.log("get...")
+
+  res.status(200).json({
+      script : _exec.join(' '),
+      result : req_info.result
+  });
+
+  req_info = {
+    python : "",
+    result : undefined,
+    script : "",
+    index : ""
+  };
+  
+});
 
 module.exports = router;
